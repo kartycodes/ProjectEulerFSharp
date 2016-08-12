764ebb4d0bf85ea488e312c921209e97f32521c1
Kartik Subramani <Kartik Subramani>
2016-08-12 19:17:44 -0400

    Solution to Probem 4

---
+++ ProjectEuler/ProjectEuler/Problem4.fs
@@ -0,0 +1,39 @@
+﻿module Problem4
+open System
+//Largest palindrome product
+//Problem 4
+//A palindromic number reads the same both ways. The largest palindrome made from the product of two 2-digit numbers is 9009 = 91 × 99.
+//Find the largest palindrome made from the product of two 3-digit numbers.
+
+let reverse (str: string) = 
+    let reversedChars =  str.ToCharArray()
+                         |> Array.rev
+    new String(reversedChars)
+
+let isPalindrome x = 
+    let strx = x.ToString()
+    let numToCompare = strx.Length / 2
+    let firstHalf = strx.Substring(0,numToCompare)
+    let secondHalfIndex = if (strx.Length % 2 = 0) then strx.Length / 2
+                          else strx.Length / 2 + 1
+    let secondHalf = strx.Substring(secondHalfIndex)
+
+    secondHalf = reverse(firstHalf)
+
+let isDivisibleByTwoThreeDigitNumbers N = 
+    let threeDigitDivisors = [100 .. 999]
+                             |> List.filter (fun i -> N % i = 0)
+                             |> List.map    (fun i -> N / i)
+                             |> List.filter (fun i -> 100 <= i && i <= 999 )
+                
+    match threeDigitDivisors with
+    | x :: tail -> true
+    | _ -> false
+                       
+let findLargestPalindrome max min = 
+    {max .. -1 .. min}
+    |> Seq.filter isPalindrome 
+    |> Seq.filter isDivisibleByTwoThreeDigitNumbers
+    |> Seq.head
+
+let solution = findLargestPalindrome (999*999) (100*100)
---
+++ ProjectEuler/ProjectEuler/Program.fs
@@ -0,0 +1,15 @@
+﻿// Learn more about F# at http://fsharp.org
+// See the 'F# Tutorial' project for more help.
+
+open Problem4
+open System.Diagnostics
+
+[<EntryPoint>]
+let main argv = 
+
+    let watch = Stopwatch.StartNew()
+    let answer = solution
+    watch.Stop()
+    printfn "The answer is: %A" answer
+    printfn "Found the answer in %A milliseconds" watch.ElapsedMilliseconds
+    0 // return an integer exit code
