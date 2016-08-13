665a74d3eb54d68b5401eb9c1c6fb8342859d814
Kartik Subramani <Kartik Subramani>
2016-08-13 18:22:35 -0400

    Added problem description to problem 7

--- ProjectEuler/ProjectEuler/Problem7.fs
+++ ProjectEuler/ProjectEuler/Problem7.fs
@@ -1,5 +1,8 @@
 ﻿module Problem7
 
+//By listing the first six prime numbers: 2, 3, 5, 7, 11, and 13, we can see that the 6th prime is 13.
+//
+//What is the 10 001st prime number?
 open Primes
 
 let solution = primes |> Seq.take 10001 |> Seq.toList |> List.item 10000
