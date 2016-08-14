ae62c073784745d0e303ca3606ea5040555f7091
Kartik Subramani <Kartik Subramani>
2016-08-13 20:26:44 -0400

    Commiting Problem10 for realsies

---
+++ ProjectEuler/ProjectEuler/Problem10.fs
@@ -0,0 +1,11 @@
+﻿module Problem10
+
+//The sum of the primes below 10 is 2 + 3 + 5 + 7 = 17.
+//
+//Find the sum of all the primes below two million.
+open Primes
+
+let sumOfPrimesLessThan x =
+    primes |> Seq.takeWhile (fun i -> i < x) |> Seq.fold (fun acc i -> acc + (int64 i)) 0L
+
+let solution = sumOfPrimesLessThan 2000000
