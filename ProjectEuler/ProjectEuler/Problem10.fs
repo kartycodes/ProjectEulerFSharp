289a684bc4a14cc1275e10b857cc353d955a9143
Kartik Subramani <Kartik Subramani>
2016-08-13 20:27:22 -0400

    Pretty printing

--- ProjectEuler/ProjectEuler/Problem10.fs
+++ ProjectEuler/ProjectEuler/Problem10.fs
@@ -6,6 +6,8 @@
 open Primes
 
 let sumOfPrimesLessThan x =
-    primes |> Seq.takeWhile (fun i -> i < x) |> Seq.fold (fun acc i -> acc + (int64 i)) 0L
+    primes 
+    |> Seq.takeWhile (fun i -> i < x) 
+    |> Seq.fold (fun acc i -> acc + (int64 i)) 0L
 
 let solution = sumOfPrimesLessThan 2000000
