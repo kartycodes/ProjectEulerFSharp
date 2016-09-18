19307e5e83de04aee28a7bb7c6ef65f3f1e8e3f5
Kartik Subramani <Kartik Subramani>
2016-09-18 18:27:36 -0400

    Added alternate solutions to P16

--- ProjectEuler/ProjectEuler/Problem16.fs
+++ ProjectEuler/ProjectEuler/Problem16.fs
@@ -1,6 +1,6 @@
 ﻿module Problem16
 
-
+// doubles an integer, when integer is represented as a list of its digits.
 let rec double (digits: int list) (carry :int) =
     match digits with
     | x :: tail -> 
@@ -19,5 +19,24 @@ let sumOfDigits n =
         digits <- double digits 0
             
     List.sum digits
+
+// alternate solution using bigint and loops
+let sumBigint n = 
+    let mutable x = bigint.Pow((bigint 2),n)
+    let mutable sum = 0
+    while (x > (bigint 0)) do
+        sum <- (int)(x % (bigint 10)) + sum
+        x <- x / (bigint 10)
+    sum
+
+// functional programming approach with bigint
+let sumBigIntRec n = 
+    let rec calcSum (b: bigint) (sum: int) =
+        match b with
+        | x when x = (bigint 0) -> sum
+        | x -> calcSum (x / (bigint 10)) ((int)(x % bigint 10) + sum)
+    calcSum (bigint.Pow((bigint 2), n)) 0 
+    
+    
         
-let solution = sumOfDigits 1000
+let solution = sumBigIntRec 1000
