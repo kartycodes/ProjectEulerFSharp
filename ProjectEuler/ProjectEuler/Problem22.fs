da1e3d133e20d91a0ba4464de532c057702697fb
Kartik Subramani <Kartik Subramani>
2016-09-24 12:10:50 -0400

    Solved Problem 22

---
+++ ProjectEuler/ProjectEuler/Problem22.fs
@@ -0,0 +1,19 @@
+﻿module Problem22
+
+open System
+
+let lines = IO.File.ReadLines @"C:\code\personal\ProjectEulerFSharp\ProjectEuler\ProjectEuler\content\p022_names.txt"
+
+let nameString = lines |> Seq.head
+
+let names = nameString.Split(',') |> Seq.map (fun (i:string) -> i.Trim('"'))
+
+let score (name:string) = 
+    let mutable sum = 0;
+    name |> String.iter (fun c -> sum <- sum + (int c)-64)
+    sum
+
+let (solution,_) = 
+    names 
+    |> Seq.sort
+    |> Seq.fold (fun (sum,idx) i -> (sum+idx*(score i),idx+1)) (0,1)
