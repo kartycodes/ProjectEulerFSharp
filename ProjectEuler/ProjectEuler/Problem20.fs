2b1e57b6075dfee636bfd7c0a58c39ef90d16272
Kartik Subramani <Kartik Subramani>
2016-09-23 23:25:54 -0400

    solved problem 21

--- ProjectEuler/ProjectEuler/Problem20.fs
+++ ProjectEuler/ProjectEuler/Problem20.fs
@@ -1,8 +1,8 @@
 ﻿module Problem20
-
+open Utils
 let rec factorial n =
     match n with 
     | x when x = (bigint 1) -> (bigint 1)
-    | x  ->  x * (factorial (x-(bigint 1))
+    | x  ->  x * (factorial (x-(bigint 1)))
 
-
+let solution = sumOfTheDigitsofABigInt (factorial (bigint 100))
